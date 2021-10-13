980dee5faa5a1459932a5b732db53494221f01c2
Marcelo Zaguette Junior <41178325+m-zaguette@users.noreply.github.com>
2021-10-13 19:53:33 -0300

    Finished Get Started from Socket.io

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hi');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);  
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {    
+      console.log('user disconnected');  
+    });
+
+     // This will emit the event to all connected sockets
+    io.emit('some event', { 
+        someProperty: 'some value', otherProperty: 'other value'
+    });
+});
+
+server.listen(3000, () => {  console.log('listening on *:3000');
+});
+
